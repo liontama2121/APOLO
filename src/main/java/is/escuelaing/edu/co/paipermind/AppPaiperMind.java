d1b8b665a7a20d558f1524e5b53d7456557bd5c7
liontama2121 <juancamolina9721@gmail.com>
2022-10-20 21:32:51 -0500

    spring

--- src/main/java/is/escuelaing/edu/co/paipermind/AppPaiperMind.java
+++ src/main/java/is/escuelaing/edu/co/paipermind/AppPaiperMind.java
@@ -1,5 +1,22 @@
 package is.escuelaing.edu.co.paipermind;
 
+import org.postgresql.util.PSQLException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan(basePackages={"is.escuelaing.edu.co.paipermind"})
 public class AppPaiperMind {
+    public static void main(String[] args) throws PSQLException {
+        try {
+            SpringApplication.run(AppPaiperMind.class, args);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
 
 }
